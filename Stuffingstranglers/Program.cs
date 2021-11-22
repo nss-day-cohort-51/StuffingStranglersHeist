88071a98162055f0edca8c089882425205b9ec5d
Devery Page <pagedevery@gmail.com>
2021-11-22 10:19:12 -0600

    initial files

---
+++ Stuffingstranglers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stuffingstranglers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Plan Your Heist");
+            Console.WriteLine("---------------");
+        }
+    }
+}
